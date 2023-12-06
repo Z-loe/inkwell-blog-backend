f599a13b280d1551a971eed15d2c8fc65f86f7be
LanceHE6 <2765543491@qq.com>
2023-12-06 22:56:20 +0800

    feat.增加分类表和文章表的创建

--- src/main/java/com/inkwell/inkwellblog/DataBase/InitSqlite.java
+++ src/main/java/com/inkwell/inkwellblog/DataBase/InitSqlite.java
@@ -6,8 +6,10 @@ public class InitSqlite {
     private static SqliteHelper sqliteHelper = null;
     public InitSqlite() throws SQLException, ClassNotFoundException {
         sqliteHelper = new SqliteHelper("inkwellDb.db");
-        String sqlCreateString = "create table if not exists User(UID text, nickname text, account text, password text, userType int, token text)";
-        sqliteHelper.executeUpdate(sqlCreateString);
+        String sqlCreateUserTable = "create table if not exists User(uid text, nickname text, account text, password text, userType int, token text)";
+        String sqlCreateCategoryTable = "create table if not exists Category(id text, name text)";
+        String sqlCreateArticleTable = "create table if not exists Article(id text, title text, content text, categoryId text, createTime text)";
+        sqliteHelper.executeUpdate(sqlCreateUserTable, sqlCreateArticleTable, sqlCreateCategoryTable);
     }
 
     static public SqliteHelper getSqliteHelper(){
