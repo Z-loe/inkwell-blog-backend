1a65469e4b748e66352b510c939b88d73a4928a2
LanceHE6 <2765543491@qq.com>
2023-12-06 17:12:10 +0800

    feat.增加uid生成算法，优化代码结构

--- src/main/java/com/inkwell/inkwellblog/API/Signup.java
+++ src/main/java/com/inkwell/inkwellblog/API/Signup.java
@@ -8,6 +8,7 @@ import com.inkwell.inkwellblog.ReturnData.UserData;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin
@@ -19,38 +20,52 @@ public class Signup {
      */
     @PostMapping("signup")
     public BaseReturnData signup(@RequestBody SignupParam param) throws SQLException, ClassNotFoundException {
-        String  account = param.getAccount();
-        String  password = param.getPassword();
+        String account = param.getAccount();
+        String password = param.getPassword();
         String nickname = param.getNickname();
+
         SqliteHelper sqliteHelper= InitSqlite.getSqliteHelper();
+
         //检查用户是否存在
         String sqlQueryString = "select count(*) from User where account = '%s'".formatted(account);
         int result = sqliteHelper.executeQuery(sqlQueryString, resultSet -> resultSet.getInt("count(*)"));
         UserData userData = new UserData();
 
         if(result!=0){
-            userData.setCode(0);
-            userData.setMessage("用户名已被占用");
+            userData.setCode(-1);
+            userData.setMessage("账号已被注册");
             return userData;
         }
 
         //设置用户数据
-        userData.setCode(1);
+        userData.setCode(200);
         userData.setMessage("注册成功");
-        String sqlQueryCountString = "select count(*) from User";
-        int resultCount = sqliteHelper.executeQuery(sqlQueryCountString, resultSet -> resultSet.getInt("count(*)"));
-        userData.setUid(resultCount+1);
+//        String sqlQueryCountString = "select count(*) from User";
+//        int resultCount = sqliteHelper.executeQuery(sqlQueryCountString, resultSet -> resultSet.getInt("count(*)"));
+        // 根据随机UUID哈希值生成UID
+        int hashCode = UUID.randomUUID().toString().hashCode();
+        if (hashCode <0){
+            hashCode=-hashCode;
+        }
+        // 0 代表前面补充0
+        // 10 代表长度为10
+        // d 代表参数为正数型
+        String uid = String.format("%010d", hashCode).substring(0,10);
+
+        userData.setUid(uid);
         userData.setAccount(account);
         userData.setUserType(0);
-        userData.setToken("hjghjk");
+        userData.setToken("");
         userData.setNickname(nickname);
 
         //添入数据库
-        int UID=userData.getUid();
+        String UID=userData.getUid();
         int userType= userData.getUserType();
         String token= userData.getToken();
-        String sqlInsertString ="insert into User VALUES(%d,'%s','%s','%s',%d,'%s')".formatted(UID,nickname,account,password,userType,token);
+
+        String sqlInsertString ="insert into User VALUES('%s','%s','%s','%s',%d,'%s')".formatted(UID,nickname,account,password,userType,token);
         sqliteHelper.executeUpdate(sqlInsertString);
+
         return userData;
     }
 }
