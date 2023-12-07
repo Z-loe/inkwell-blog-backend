6f604e9d802ca6c27f4b618117c30a44232a8b18
LanceHE6 <2765543491@qq.com>
2023-12-07 21:50:51 +0800

    feat.增加token鉴权

--- src/main/java/com/inkwell/inkwellblog/API/Category/Update.java
+++ src/main/java/com/inkwell/inkwellblog/API/Category/Update.java
@@ -4,6 +4,7 @@ import com.inkwell.inkwellblog.DataBase.InitSqlite;
 import com.inkwell.inkwellblog.DataBase.SqliteHelper;
 import com.inkwell.inkwellblog.RequestParam.UpdateParam;
 import com.inkwell.inkwellblog.ReturnData.BaseReturnData;
+import com.inkwell.inkwellblog.Util.TokenAuthenticate;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
@@ -14,7 +15,22 @@ import java.util.Objects;
 @RequestMapping("category")
 public class Update {
     @PostMapping("update")
-    public BaseReturnData update(UpdateParam param) throws SQLException, ClassNotFoundException {
+    public BaseReturnData update(@RequestBody UpdateParam param, @RequestHeader("token") String token) throws SQLException, ClassNotFoundException {
+
+        // token鉴权
+        int checkResult = TokenAuthenticate.checkToken(token);
+        if (checkResult == -1){
+            BaseReturnData returnData = new BaseReturnData();
+            returnData.setCode(403);
+            returnData.setMessage("请先登录");
+            return returnData;
+        } else if(checkResult == 0){
+            BaseReturnData returnData = new BaseReturnData();
+            returnData.setCode(403);
+            returnData.setMessage("您没有权限执行此操作");
+            return returnData;
+        }
+
         SqliteHelper sqliteHelper = InitSqlite.getSqliteHelper();
         BaseReturnData baseReturnData = new BaseReturnData();
         String getInfoSql = "select id from Category";
