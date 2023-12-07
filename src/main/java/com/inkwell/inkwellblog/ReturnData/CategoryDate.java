4a5d1a110bc36cb526b16b4514df0a4ebdd3e579
Z-loe <1472390784@qq.com>
2023-12-07 10:07:57 +0800

    新增目录数据类

---
+++ src/main/java/com/inkwell/inkwellblog/ReturnData/CategoryDate.java
@@ -0,0 +1,23 @@
+package com.inkwell.inkwellblog.ReturnData;
+
+public class CategoryDate {
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
