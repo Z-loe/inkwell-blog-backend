4ab850731d0c7b8cc228fb5cfb76bf703fb8ddef
XDyoroshiku <2276341011@qq.com>
2023-12-07 11:26:47 +0800

    增加文章类

---
+++ src/main/java/com/inkwell/inkwellblog/RequestParam/AddParam.java
@@ -0,0 +1,17 @@
+package com.inkwell.inkwellblog.RequestParam;
+
+import javax.naming.Name;
+
+public class AddParam
+
+{
+    private String name;
+
+    public void setName(String account){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
