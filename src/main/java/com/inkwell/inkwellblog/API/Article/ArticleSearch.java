31bd3537ab937d135699f82b7ddf178e69f4d4ad
LanceHE6 <2765543491@qq.com>
2023-12-10 21:01:45 +0800

    refactor.优化传参及默认值设置，优化算法

--- src/main/java/com/inkwell/inkwellblog/API/Article/ArticleSearch.java
+++ src/main/java/com/inkwell/inkwellblog/API/Article/ArticleSearch.java
@@ -1,107 +1,96 @@
 package com.inkwell.inkwellblog.API.Article;
 
-import com.inkwell.inkwellblog.DataBase.InitSqlite;
 import com.inkwell.inkwellblog.DataBase.SqliteHelper;
-import com.inkwell.inkwellblog.RequestParam.SearchArticleParam;
-import com.inkwell.inkwellblog.ReturnData.ArticleSearchData;
-import com.inkwell.inkwellblog.ReturnData.BaseReturnData;
-import com.inkwell.inkwellblog.ReturnData.DetailData;
-import com.inkwell.inkwellblog.ReturnData.SearchData;
+import com.inkwell.inkwellblog.Util.Constants;
 import com.inkwell.inkwellblog.Util.TokenAuthenticate;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @CrossOrigin
 @RequestMapping("article")
 public class ArticleSearch {
     @GetMapping("search")
-    public BaseReturnData articleSearch(@RequestBody SearchArticleParam param, @RequestHeader("token") String token) throws SQLException, ClassNotFoundException {
-
-        //token鉴权
+    public Map<String, Object> searchArticles(
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false) String categoryId,
+            @RequestParam(defaultValue = "1", required = false) int page,
+            @RequestParam(defaultValue = "10", required = false) int pageSize,
+            @RequestHeader("token") String token
+    ) throws SQLException, ClassNotFoundException {
+        // token鉴权
         int checkResult = TokenAuthenticate.checkToken(token);
-        if (checkResult == -1) {
-            BaseReturnData returnData = new BaseReturnData();
-            returnData.setCode(403);
-            returnData.setMessage("请先登录");
+        if (checkResult == -1){
+            Map<String, Object> returnData = new HashMap<>();
+            returnData.put("code", 403);
+            returnData.put("message", "请先登录");
             return returnData;
-        } else if (checkResult == 0) {
-            BaseReturnData returnData = new BaseReturnData();
-            returnData.setCode(403);
-            returnData.setMessage("您没有权限执行此操作");
+        } else if(checkResult == 0){
+            Map<String, Object> returnData = new HashMap<>();
+            returnData.put("code", 403);
+            returnData.put("message", "您没有权限执行此操作");
             return returnData;
         }
 
-        SqliteHelper sqliteHelper = InitSqlite.getSqliteHelper();
-        ArticleSearchData articleSearchData = new ArticleSearchData();
+        SqliteHelper sqliteHelper = new SqliteHelper(Constants.DATABASE_PATH);
+        try {
+            // 构建 SQL 查询语句
+            String sql = "SELECT id, title, substr(content, 0, 50) as content, categoryId, createTime FROM Article WHERE 1=1";
+            if (keyword != null) {
+                sql += " AND title LIKE '%" + keyword + "%'";
+            }
+            if (categoryId != null) {
+                sql += " AND categoryId = '" + categoryId + "'";
+            }
+            sql += " LIMIT " + pageSize + " OFFSET " + (page - 1) * pageSize;
 
-        //默认值检测
-        int page = param.getPage() == 0 ? 1 : param.getPage();
-        int pageSize = param.getPageSize() == 0 ? 10 : param.getPageSize();
-        String categoryId = param.getCategoryId() == null ? "" : param.getCategoryId();
-        String keyword = param.getKeyword() == null ? "" : param.getKeyword();
+            // 执行查询并处理结果
+            List<Map<String, Object>> articleList = sqliteHelper.executeQuery(sql, rs -> {
+                List<Map<String, Object>> result = new ArrayList<>();
+                while (rs.next()) {
+                    Map<String, Object> article = new HashMap<>();
+                    article.put("id", rs.getString("id"));
+                    article.put("title", rs.getString("title"));
+                    article.put("categoryId", rs.getString("categoryId"));
+                    article.put("createTime", rs.getString("createTime"));
+                    article.put("content", rs.getString("content"));
+                    result.add(article);
+                }
+                return result;
+            });
 
-        //构建sql语句
-        String sqlSearch = "select * from Article ";
-        boolean flag = true;
-        if (!Objects.equals(keyword, "")) {
-            sqlSearch += "where ";
-            flag = false;
-            sqlSearch += "(title like '%Keyword%' or content like '%Keyword%') ".replace("Keyword", param.getKeyword());
+            // 查询总数
+            String countSql = "SELECT COUNT(*) AS count FROM Article WHERE 1=1";
+            if (keyword != null) {
+                countSql += " AND title LIKE '%" + keyword + "%'";
+            }
+            if (categoryId != null) {
+                countSql += " AND categoryId = '" + categoryId + "'";
+            }
+            int count = sqliteHelper.executeQuery(countSql, rs -> rs.getInt("count"));
+
+            // 构建返回结果
+            Map<String, Object> response = new HashMap<>();
+            response.put("code", 200);
+            response.put("message", "查询成功");
+            response.put("data", new HashMap<String, Object>() {{
+                put("keyword", keyword);
+                put("category", categoryId);
+                put("page", page);
+                put("pageSize", pageSize);
+                put("rows", articleList);
+                put("count", count);
+            }});
+            return response;
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 处理异常情况
+            Map<String, Object> response = new HashMap<>();
+            response.put("code", 500);
+            response.put("message", "查询失败: " + e.getMessage());
+            return response;
         }
-
-        if (!Objects.equals(categoryId, "")) {
-            if (flag) {
-                sqlSearch += "where ";
-            } else {
-                sqlSearch += "and ";
-            }
-            sqlSearch += "categoryId ='%s' ".formatted(param.getCategoryId());
-        }
-        sqlSearch += "limit (%d*%d),%d".formatted(page-1, pageSize, pageSize);
-        String sqlSearchCount="select count(*) from ( "+ sqlSearch+")";
-        final int[] count = new int[1];
-
-        //查询
-        sqliteHelper.executeQuery(sqlSearchCount, resultSet -> {
-            count[0] = resultSet.getInt("count(*)");
-            if(count[0] ==0){
-                articleSearchData.setCode(201);
-                articleSearchData.setMessage("没有匹配的结果");
-            }
-            return null;
-        });
-        sqliteHelper.executeQuery(sqlSearch, resultSet -> {
-            DetailData[] detailData = new DetailData[count[0]];
-            int i = 0;
-            while (resultSet.next()) {
-
-                String id = resultSet.getString("id");
-                String title = resultSet.getString("title");
-                String categoryID = resultSet.getString("categoryId");
-                String createTime = resultSet.getString("createTime");
-
-                // 将获取的数据设置到articleData对象中
-                detailData[i] = new DetailData();
-                detailData[i].setId(id);
-                detailData[i].setTitle(title);
-                detailData[i].setCategoryId(categoryID);
-                detailData[i].setCreateTime(createTime);
-                i++;
-            }
-            SearchData searchData =new SearchData();
-            searchData.setKeyword(param.getKeyword());
-            searchData.setCategoryId(param.getCategoryId());
-            searchData.setPage(page);
-            searchData.setPageSize(pageSize);
-            searchData.setRows(detailData);
-            articleSearchData.setCode(200);
-            articleSearchData.setMessage("查询成功");
-            articleSearchData.setData(searchData);
-            return null;
-        });
-        return articleSearchData;
     }
 }
