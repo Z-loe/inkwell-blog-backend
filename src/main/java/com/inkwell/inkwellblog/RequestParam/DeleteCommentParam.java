2e2741cc348fa71260edef597d2956966ab04da7
XDyoroshiku <2276341011@qq.com>
2023-12-11 11:29:32 +0800

    deletecommentparam

---
+++ src/main/java/com/inkwell/inkwellblog/RequestParam/DeleteCommentParam.java
@@ -0,0 +1,21 @@
+package com.inkwell.inkwellblog.RequestParam;
+
+public class DeleteCommentParam
+{
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {this.id = id;}
+
+
+    private String cid;
+
+    public String getCid() {
+        return cid;
+    }
+
+    public void setCid(String cid) {this.cid = cid;}
+}
