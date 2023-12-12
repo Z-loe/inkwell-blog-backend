97d57ad3a971087708887de48d8690ba5b602e82
Z-loe <1472390784@qq.com>
2023-12-12 08:46:20 +0800

    feat.添加发表评论及其相关参数类

---
+++ src/main/java/com/inkwell/inkwellblog/API/Comment/CommentPost.java
@@ -0,0 +1,57 @@
+package com.inkwell.inkwellblog.API.Comment;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.inkwell.inkwellblog.DataBase.SqliteHelper;
+import com.inkwell.inkwellblog.RequestParam.PostCommentParam;
+import com.inkwell.inkwellblog.ReturnData.BaseReturnData;
+import com.inkwell.inkwellblog.Util.Constants;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+@RestController
+@CrossOrigin
+@RequestMapping("comment")
+public class CommentPost {
+    @PostMapping("post")
+    public BaseReturnData post(PostCommentParam param) throws SQLException, ClassNotFoundException, JsonProcessingException {
+
+
+        SqliteHelper sqliteHelper = new SqliteHelper(Constants.DATABASE_PATH);
+        String id = param.getId();
+        String uid = param.getUid();
+        String commentParam = param.getComment();
+        String commentString="{ id: "+id+",uid: "+uid+",content: "+commentParam+" }";
+
+        String sqlQuery = "SELECT comment FROM Article WHERE id= "+id;
+        sqliteHelper.executeQuery(sqlQuery, resultSet -> {
+            String comment = resultSet.getString("comment");
+            String commentList;
+            if (comment == null) {
+                commentList ="{"+commentString+"}";
+            }
+            else {
+                commentList= comment.substring(0, comment.length() - 1) + ","+commentString+"}";
+            }
+
+            //插入数据到表
+            String sqlUpdate = "update Article set comment='" + commentList +  "' where id= "+id;
+            try {
+                sqliteHelper.executeUpdate(sqlUpdate);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        });
+
+        //返回状态码
+        BaseReturnData returnData = new BaseReturnData();
+        returnData.setCode(200);
+        returnData.setMessage("发表成功");
+        sqliteHelper.destroyed();
+        return returnData;
+    }
+}
+
