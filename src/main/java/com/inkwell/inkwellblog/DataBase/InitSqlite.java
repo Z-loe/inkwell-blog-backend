3060e164dd72576c865202414a29efae63a7f674
Z-loe <1472390784@qq.com>
2023-12-12 09:11:21 +0800

    feat.添加用户头像字段

--- src/main/java/com/inkwell/inkwellblog/DataBase/InitSqlite.java
+++ src/main/java/com/inkwell/inkwellblog/DataBase/InitSqlite.java
@@ -7,16 +7,11 @@ public class InitSqlite {
     public InitSqlite() throws SQLException, ClassNotFoundException {
         System.out.println("数据表检查...");
         SqliteHelper sqliteHelper = new SqliteHelper("inkwellDb.db");
-        String sqlCreateUserTable = "create table if not exists User(uid text, nickname text, account text, password text, userType int, token text)";
+        String sqlCreateUserTable = "create table if not exists User(uid text, nickname text, account text, password text, userType int, token text, avatar text)";
         String sqlCreateCategoryTable = "create table if not exists Category(id text, name text)";
         String sqlCreateArticleTable = "create table if not exists Article(id text, title text, content text, categoryId text, createTime text, comment text)";
         sqliteHelper.executeUpdate(sqlCreateUserTable, sqlCreateArticleTable, sqlCreateCategoryTable);
         sqliteHelper.destroyed();
         System.out.println("数据表检查完成");
     }
-
-//   // static public SqliteHelper getSqliteHelper(){
-//        return sqliteHelper;
-//    }
-
 }
