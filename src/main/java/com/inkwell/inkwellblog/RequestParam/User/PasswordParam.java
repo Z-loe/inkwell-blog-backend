7e3de271cfd5f3072db3a7bbf12496a7eedafd07
Z-loe <1472390784@qq.com>
2023-12-13 11:17:39 +0800

    feat.新增用户密码修改和参数类

---
+++ src/main/java/com/inkwell/inkwellblog/RequestParam/User/PasswordParam.java
@@ -0,0 +1,22 @@
+package com.inkwell.inkwellblog.RequestParam.User;
+
+public class PasswordParam {
+    private String uid;
+    private String password;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
